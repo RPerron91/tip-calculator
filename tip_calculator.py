a47c98a2886263e2317ae69e6cecba10c9a5955b
Robert <rperron91@gmail.com>
2019-05-10 18:23:02 -0400

    Initial Commit
    Initial Commit for tip calculator

---
+++ tip_calculator.py
@@ -0,0 +1,36 @@
+#Recursive function to get bill with valid type
+def getBill():
+    #Set bill equal to user input
+    Bill = input("Enter Bill Total: ")
+    #Exception handling and converting Bill from string to float
+    try:
+        #Change input to float and return Bill with no whitespace
+        Bill = float(f"{Bill}\n")
+        #Checks for non-infinite, positive value
+        if Bill >= 0 and Bill != float('inf'):
+            print("Bill Total: $", Bill)
+            return Bill
+        else:
+            print("Enter Positive, non-infinite Value for Bill")
+            getBill()
+
+    except ValueError:
+        print("Input valid number for Bill")
+        getBill()
+getBill()
+
+
+def numPeople():
+    people = input("How many people are splitting the bill?")
+    #Exception handling for converting Bill from string to float
+    try:
+        #Change input to float and return
+        int(people)
+        print("Total People: ", people)
+        return people
+    except ValueError:
+        print("Input valid number of people")
+        numPeople()
+    
+
+
