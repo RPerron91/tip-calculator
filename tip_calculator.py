67373e09a79daec836d37df5b774c8bb4a41f855
Robert <rperron91@gmail.com>
2019-05-10 22:52:07 -0400

    tip calculator almost works
    Bugs when handling exceptions. Code runs if all parameters are correct

--- tip_calculator.py
+++ tip_calculator.py
@@ -17,8 +17,6 @@ def getBill():
     except ValueError:
         print("Input valid number for Bill")
         getBill()
-getBill()
-
 
 def numPeople():
     people = input("How many people are splitting the bill?")
@@ -36,6 +34,35 @@ def numPeople():
     except ValueError:
         print("Input valid whole number of people")
         numPeople()
-numPeople()   
 
+def percentTip():
+    #Set tip equal to user input
+    Tip = input("% Tip:")
+    #Exception handling and converting tip from string to float
+    try:
+        #Change input to float and return tip with no whitespace
+        Tip = float(f"{Tip}\n")
+        #Checks for non-infinite, positive value
+        if Tip > 0 and Tip != float('inf'):
+            print("Tip Percent: %", Tip)
+            return Tip
+        else:
+            print("Enter Positive, Non-Infinite Value for Tip")
+            percentTip()
 
+    except ValueError:
+        print("Input valid number for tip")
+        percentTip()
+
+#Set Functions as Variables to be Passed into Last Function
+bill = getBill()
+people = numPeople()
+tip = percentTip()
+
+def splitBill(bill, people, tip):
+    tipAmount = bill*(tip/100)
+    eachTip = round(tipAmount/people, 2)
+    print(f"Each person should tip ${eachTip}")
+    return eachTip
+
+splitBill(bill, people, tip)
